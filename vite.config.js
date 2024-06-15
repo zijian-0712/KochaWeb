188bb0386ef96f9316c0bb4162331171b229f637
zijian-0712 <153837594+zijian-0712@users.noreply.github.com>
2024-06-15 17:08:37 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'/viteTest2/',
+  base:'/KochaWeb/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
