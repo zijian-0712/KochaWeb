b1a7a3be3a66078f78ba0ed0a3ece0c71371f826
zijian-0712 <153837594+zijian-0712@users.noreply.github.com>
2024-06-15 15:39:04 +0800

    1

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {reactive, ref} from "vue";
+
+let a=ref({num:1})
+function click(){
+  a.value.num++;
+}
+</script>
+
+<template>
+<button v-on:click="click">{{a.num}}</button>
+</template>
+
+<style scoped>
+
+</style>
