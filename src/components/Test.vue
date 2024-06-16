9fc95110e313e9e6f02cab8ef209c8552b17f098
zijian-0712 <153837594+zijian-0712@users.noreply.github.com>
2024-06-16 16:01:06 +0800

    1

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,20 +1,18 @@
 <script setup>
-import {reactive, ref} from "vue";
+import { reactive, ref } from "vue";
 
-let a=ref({
-  name:"name",
-  count:0
-})
-function click(){
-a.value.count++
+let a = ref({
+  name: "name",
+  count: 0,
+});
+function click() {
+  a.value.count++;
 }
 </script>
 
 <template>
-  <p>  {{a.name}}</p>
-<button v-on:click="click">{{a.count}}</button>
+  <p>{{ a.name }}</p>
+  <button v-on:click="click">{{ a.count }}</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
